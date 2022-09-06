1787814e557d7cbb2ad929258d2832fd31e13b63
Aubree Herrion <aubreeherrion@gmail.com>
2022-09-06 17:26:41 -0500

    add MUI components

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,5 +1,10 @@
 import {useState, useEffect} from 'react';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid'; 
+import Card from '@mui/material/Card'; 
+import CardActions from '@mui/material/CardActions'; 
+import CardMedia from '@mui/material/CardMedia'; 
+import Typography from '@mui/material/Typography';
 import './GalleryItem.css';
 
 
@@ -7,31 +12,46 @@ import './GalleryItem.css';
 function GalleryItem({photo, putGalleryList, }){
     const [toggleDescription, setToggleDescription] = useState(false)
 
+    return(
+        <Grid item sm={6} xl={4}>
+            <Card className='card' sx={{borderRadius: '5%'}} elevation={5}>
+                <div className="cardInfo" onClick={() => setToggleDescription(!toggleDescription)}>
+                    {toggleDescription ?( <Typography className='cardDescription'>{photo.description}</Typography>)
+                    : (<CardMedia component="img" height= "300px" image={photo.path} />)}
+                </div>
+                <CardActions>
+                    <Typography>Like count: {photo.likes}</Typography>
+                    <Button onClick={() => putGalleryList(photo.id)}>Like It!</Button>
+                </CardActions>
+            </Card>
+        </Grid>
+    )
+
     // const onSubmit = () => {
     //     putGalleryList(photo.id);
     // };
-    const seeDescription = () => {
-        setToggleDescription(!toggleDescription)
-    };
-    const seePhoto = () =>{
-        if (toggleDescription){
-            return <img src={photo.path}></img>
-        }
-        else{
-            return <p>{photo.description}</p>
-        }
-    }
+    // const seeDescription = () => {
+    //     setToggleDescription(!toggleDescription)
+    // };
+    // const seePhoto = () =>{
+    //     if (toggleDescription){
+    //         return <img src={photo.path}></img>
+    //     }
+    //     else{
+    //         return <p>{photo.description}</p>
+    //     }
+    // }
 
-    return(
-        <div>
-            <li onClick={seeDescription}>
-                {seePhoto()}
+    // return(
+    //     <div>
+    //         <li onClick={seeDescription}>
+    //             {seePhoto()}
   
-            </li>
-            Like:{photo.likes}
-                <Button  onClick={() => putGalleryList(photo.id)}>Like It!</Button>
-        </div>
-    )
+    //         </li>
+    //         Like:{photo.likes}
+    //             <Button  onClick={() => putGalleryList(photo.id)}>Like It!</Button>
+    //     </div>
+    // )
 
 }
 export default GalleryItem;
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,21 +1,29 @@
 import {useState, useEffect } from "react";
+import Grid from '@mui/material/Grid';
 import GalleryItem from "../GalleryItem/GalleryItem";
+import { PhotoAlbumSharp } from "@mui/icons-material";
 
 function GalleryList({galleryList, putGalleryList,}){
 
 
     // returns list from array
     return (
-        <ul>
-            <li>
-                {galleryList.map ((photo) => {
-                    return <GalleryItem key = {photo.id}
-                    photo = {photo} 
-                    putGalleryList = {putGalleryList} />
+        <Grid container spacing={3}>
+            {galleryList.map((photo) => {
+                return <GalleryItem key={photo.id} photo={photo} putGalleryList = {putGalleryList} />
+            })}
+        </Grid>
+
+        // <ul>
+        //     <li>
+        //         {galleryList.map ((photo) => {
+        //             return <GalleryItem key = {photo.id}
+        //             photo = {photo} 
+        //             putGalleryList = {putGalleryList} />
                 
-                })}
-            </li>
-        </ul>
+        //         })}
+        //     </li>
+        // </ul>
     )
 }
  export default GalleryList;
